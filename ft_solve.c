bf30062681cd88ece122a097178a22f6158e8992
Igor Tiievskyi <it@MacBook-Air-Igor.local>
2018-04-04 00:50:30 +0300

    Minor updates (00:50)

--- ft_solve.c
+++ ft_solve.c
@@ -14,16 +14,16 @@
 #include "fillit.h"
 #include <stdio.h>
 
-static void			erase_map(int z[], t_tetr_list *list, char **arr)
+static void			erase_map(int size, t_tetr_list *list, char **arr)
 {
 	int		x;
 	int		l;
 
 	x = 0;
-	while(x < z[5])
+	while(x < size)
 	{
 		l = 0;
-		while (l < z[5])
+		while (l < size)
 		{
 			if (arr[x][l] == list->c)
 				arr[x][l] = '.';
@@ -31,76 +31,72 @@ static void			erase_map(int z[], t_tetr_list *list, char **arr)
 		}
 		x++;
 	}
-	z[3] = 0;
 }
 
-static int			is_fit(int z[], t_tetr_list *l, char **arr)
+static int			is_fit(int i, t_tetr_list *l, char **arr, int size)
 {
-	erase_map(z, l, arr);
-	if (arr[z[0] + (l->y)[0]][z[1] + (l->x)[0]] == '.' &&
-		arr[z[0] + (l->y)[1]][z[1] + (l->x)[1]] == '.' &&
-		arr[z[0] + (l->y)[2]][z[1] + (l->x)[2]] == '.' &&
-		arr[z[0] + (l->y)[3]][z[1] + (l->x)[3]] == '.')
+	int	x1;
+	int	y1;
+
+	x1 = i % 4;
+	y1 = i / 4;
+	erase_map(size, l, arr);
+	if ((l->y)[0] + y1 >= size || (l->y)[1] + y1 >= size ||
+		(l->y)[2] + y1 >= size || (l->y)[3] + y1 >= size ||
+		(l->x)[0] + x1 >= size || (l->x)[1] + x1 >= size ||
+		(l->x)[2] + x1 >= size || (l->x)[3] + x1 >= size)
+		return (0);
+	if (arr[y1 + (l->y)[0]][x1 + (l->x)[0]] == '.' &&
+		arr[y1 + (l->y)[1]][x1 + (l->x)[1]] == '.' &&
+		arr[y1 + (l->y)[2]][x1 + (l->x)[2]] == '.' &&
+		arr[y1 + (l->y)[3]][x1 + (l->x)[3]] == '.')
 	{
-		arr[z[0] + (l->y)[0]][z[1] + (l->x)[0]] = l->c;
-		arr[z[0] + (l->y)[1]][z[1] + (l->x)[1]] = l->c;
-		arr[z[0] + (l->y)[2]][z[1] + (l->x)[2]] = l->c;
-		arr[z[0] + (l->y)[3]][z[1] + (l->x)[3]] = l->c;
+		arr[y1 + (l->y)[0]][x1 + (l->x)[0]] = l->c;
+		arr[y1 + (l->y)[1]][x1 + (l->x)[1]] = l->c;
+		arr[y1 + (l->y)[2]][x1 + (l->x)[2]] = l->c;
+		arr[y1 + (l->y)[3]][x1 + (l->x)[3]] = l->c;
 		return (1);
 	}
 	else
-		return (-1);
+		printf("%c = WRONG\n", l->c);
+		return (0);
 }
 
 
-static int			place_tetr(int z[], t_tetr_list *list, char **arr)
+static int			place_tetr(int size, t_tetr_list *list, char **arr)
 {
-	int v[2];
-	ft_bzero(v, 8);
-	z[0] = 0;
-	z[1] = 0;
-	z[3] = 0;
+	int i;
+	int solve;
 
-	while (z[3] != 1 && z[1] < z[5] && z[0] < z[5] && z[4] != 1)
+	i = 0;
+	solve = 0;
+	while ((i < size * size) && solve == 0)
 	{
-		z[0] = v[0];
-		z[1] = v[1];
-		while (! && z[1] < z[5] && z[0] < z[5])
+
+		if (is_fit(i, list, arr, size) == 1)
 		{
-			is_fit(z, list, arr);
-			z[1]++;
-			if(z[1] >= z[5])
-			{
-				z[1] = 0;
-				z[0]++;
-				if (z[0] >= z[5] || z[1] >= z[5])
-					break;
-			}
+			if (list->next != NULL)
+				solve = place_tetr(size, list->next, arr);
+			else
+				return (1);
 		}
-		v[0] = z[0];
-		v[1] = z[1];
-		if (list->next != NULL && z[3] == 1)
-			place_tetr(z, list->next, arr);
-		if (z[6] - 1 == (int)((list)->c - 'A') && z[3] == 1)
-			z[4] = 1;
+		i++;
 	}
-	return (z[4]);
+	return (0);
 }
 
 char				**ft_solve(t_tetr_list **list, int p[], int a)
 {
 	char		**arr;
-	t_tetr_list	*l;
 	int 		solve;
 	int			size;
 
-	size = p[6] + a;
+	size = a + p[6];
 	solve = 0;
-	l = *list;
 	while (solve != 1 && size < 12)
 	{
-		arr = ft_malloc_arr(p[6]);
-		solve = place_tetr(z, l, arr);
+		arr = ft_malloc_arr(size);
+		solve = place_tetr(size, *list, arr);
 		size++;
 	}
 	return (arr);
